f1c5e123a9957543440913676e145f13dda49b0d
Sarit Bassal <saritbassal@Sarits-MacBook-Air.local>
2016-10-13 13:12:59 -0400

    Add name to page

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,6 @@
 /* GET home page */
 
 module.exports.index = function(req, res) {
-  res.render('index', { title: 'Express' }); /*render is the Express function for compiling a view template
+  res.render('index', { title: 'FashBash' }); /*render is the Express function for compiling a view template
   												to send as the HTML response that the browser will receive */
 };
