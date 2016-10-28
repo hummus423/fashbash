227c2338936cff98845bf8f678df5aa398eab409
Sarit Bassal <saritbassal@Sarits-Air.home>
2016-10-27 21:48:40 -0400

    Adding the view templates

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var ctrlMain = require('../controllers/main');
+var ctrlOthers = require('../controllers/others')
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
 
